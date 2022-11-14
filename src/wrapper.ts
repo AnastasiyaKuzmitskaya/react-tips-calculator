3a480d170c1f03fad02533e8fba5690147e95eb7
AnastasiyaKuzmitskaya <nataliletova2@gmail.com>
2022-11-14 23:55:09 +0300

    fix style-wrapper

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -5,6 +5,7 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
+  width: 100vw;
+  height: 100vh;
   background: no-repeat #eaf2f2 url(${shape}) top left;
 `;
